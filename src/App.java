c58bc277e5aa7596b6355d53826c587366e77a90
Priyanka <priyankaag.0613@gmail.com>
2023-12-27 23:12:51 +0530

    UC1 changes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Linked list");
+
+        List<Integer> list=new ArrayList<Integer>();  
+        list.add(56);
+        list.add(30);
+        list.add(70);
     }
 }
